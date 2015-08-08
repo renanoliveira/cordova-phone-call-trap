1b906187d77fe690d87f1892e15769a94236d5e9
Elie <contact@eliesauveterre.com>
2015-08-08 14:36:31 -0400

    Add CoreTelephony

--- src/ios/CDVPhoneCallTrap.m
+++ src/ios/CDVPhoneCallTrap.m
@@ -1,33 +1,48 @@
-/*
- Licensed to the Apache Software Foundation (ASF) under one
- or more contributor license agreements.  See the NOTICE file
- distributed with this work for additional information
- regarding copyright ownership.  The ASF licenses this file
- to you under the Apache License, Version 2.0 (the
- "License"); you may not use this file except in compliance
- with the License.  You may obtain a copy of the License at
 
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing,
- software distributed under the License is distributed on an
- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- KIND, either express or implied.  See the License for the
- specific language governing permissions and limitations
- under the License.
- */
-
-/*
- NOTE: plugman/cordova cli should have already installed this,
- but you need the value UIViewControllerBasedStatusBarAppearance
- in your Info.plist as well to set the styles in iOS 7
- */
 
 #import "CDVPhoneCallTrap.h"
+#import <CoreTelephony/CTCall.h>
 
 
 @implementation CDVPhoneCallTrap
 
+//Initialize the plugin
+- (void)pluginInitialize
+{
+    self.callCenter = [[CTCallCenter alloc] init];
+    [self handleCall];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(callReceived:) name:CTCallStateIncoming object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(callEnded:) name:CTCallStateDisconnected object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(callConnected:) name:CTCallStateConnected object:nil];
+}
 
+
+//handle calls
+-(void)handleCall
+{
+    self.callCenter.callEventHandler = ^(CTCall *call){
+        
+        if ([call.callState isEqualToString: CTCallStateConnected])
+        {
+            NSLog(@"call CTCallStateConnected");//Background task stopped
+        }
+        else if ([call.callState isEqualToString: CTCallStateDialing])
+        {
+            NSLog(@"call CTCallStateDialing");
+        }
+        else if ([call.callState isEqualToString: CTCallStateDisconnected])
+        {
+            NSLog(@"call CTCallStateDisconnected");//Background task started
+        }
+        else if ([call.callState isEqualToString: CTCallStateIncoming])
+        {
+            NSLog(@"call CTCallStateIncoming");
+        }
+        else  {
+            NSLog(@"call NO");
+        }
+    };
+}
 
 @end
