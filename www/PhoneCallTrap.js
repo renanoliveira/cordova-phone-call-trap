76a582a5902723b26d35d4fa5f5974e823157c28
Alko Schuster <alko@mobilea.nl>
2017-03-23 10:52:02 +0100

    - Added possibility to request the current PhoneCallState of the device without having to listen to the `onCall` event.

--- www/PhoneCallTrap.js
+++ www/PhoneCallTrap.js
@@ -8,8 +8,9 @@ var PhoneCallTrap = {
         console.log("WARNING: PhoneCallTrap errorCallback not implemented");
     },
 
-    getCurrentState: function(callback) {
-        cordova.exec(callback, this.errorCallback, 'PhoneCallTrap', 'getCurrentState', []);
+    getCurrentState: function(successCallback, errorCallback) {
+        errorCallback = errorCallback || this.errorCallback;
+        cordova.exec(successCallback, errorCallback, 'PhoneCallTrap', 'getCurrentState', []);
     }
 };
 
