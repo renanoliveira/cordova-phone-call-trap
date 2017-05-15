d84da76427a37a22f815e9ceb7762ddef8fd481a
Arda Beyazoğlu <arda@beyazoglu.com>
2017-05-15 18:59:12 +0200

    added caller id support and updated plugin result

--- src/android/PhoneCallTrap.java
+++ src/android/PhoneCallTrap.java
@@ -9,7 +9,7 @@ import android.telephony.TelephonyManager;
 
 import org.json.JSONException;
 import org.json.JSONArray;
-
+import org.json.JSONObject;
 
 public class PhoneCallTrap extends CordovaPlugin {
 
@@ -60,9 +60,18 @@ class CallStateListener extends PhoneStateListener {
             msg = "RINGING";
             break;
         }
+		
+        // add number info
+		JSONObject callResult = new JSONObject();
+		try {
+			callResult.put("state", msg);
+			callResult.put("number", incomingNumber);
+		}
+        catch(JSONException e){
+			
+		}
 
-        // send state and number together, and parse in javascript
-        PluginResult result = new PluginResult(PluginResult.Status.OK, msg + ":" + incomingNumber);
+        PluginResult result = new PluginResult(PluginResult.Status.OK, callResult);
         result.setKeepCallback(true);
 
         callbackContext.sendPluginResult(result);
